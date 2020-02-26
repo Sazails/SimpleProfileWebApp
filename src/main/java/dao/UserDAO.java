3ee5a8373939601e95b223044f50e4ad39d4a582
Sazails <armanis999@gmail.com>
2020-02-26 20:50:04 +0000

    Initial commit

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -4,7 +4,7 @@ import model.User;
 
 public interface UserDAO {
     boolean registerUser(User user);
-    boolean userExists(String username); // Use username because the database can only hold one type of username ignoring uppercase/lowercase differences
+    boolean userExists(String username);
 //    boolean updateUser(User user);
 //    User getUserByUsername(String username);
 //    User getUserByEmail(String email);

e987b91b2c25e3b66bd112a0d82d9c4ecab264a0
Sazails <armanis999@gmail.com>
2020-03-14 21:12:35 +0000

    Get user by email.

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -5,7 +5,7 @@ import model.User;
 public interface UserDAO {
     boolean registerUser(User user);
     boolean userExists(String username);
+    User getUserByUsername(String username);
+    User getUserByEmail(String email);
 //    boolean updateUser(User user);
-//    User getUserByUsername(String username);
-//    User getUserByEmail(String email);
 }
